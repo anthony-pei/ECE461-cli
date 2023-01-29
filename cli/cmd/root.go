80527deef49fd69bc91d0cc01f6276f5c17dc565
Anthony Pei <peiantho@gmail.com>
2023-01-29 16:21:33 -0500

    successful get repo request in root.go

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -6,11 +6,22 @@ package cmd
 
 import (
 	"os"
-
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"github.com/spf13/cobra"
+	"github.com/joho/godotenv"
 )
 
 
+func getEnvVar(key string) string {
+	err := godotenv.Load(".env")
+  if err != nil {
+    log.Fatal("Error loading .env file")
+  }
+	return os.Getenv(key)
+}
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -24,7 +35,28 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) { 
+		client := &http.Client{}
+		req, err := http.NewRequest("GET", args[0], nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+		req.Header.Set("Accept", "application/vnd.github+json")
+		req.Header.Set("Authorization", "Bearer " + getEnvVar("PERSONAL_TOKEN"))
+		req.Header.Set("X-GitHub-Api-Version", "2022-11-28")
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer resp.Body.Close()
+		
+		bodyText, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%stest\n", bodyText)
+
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
--- cli/main.go
+++ cli/main.go
@@ -4,7 +4,11 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/anthony-pei/ECE461/cli/cmd"
+import (
+	"github.com/anthony-pei/ECE461/cli/cmd"
+)
+
+
 
 func main() {
 	cmd.Execute()
