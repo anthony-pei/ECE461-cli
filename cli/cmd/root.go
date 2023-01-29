7043cedc3c546331e341c04992cb0d4e55cfd5f6
Anthony Pei <peiantho@gmail.com>
2023-01-29 17:59:58 -0500

    added some comments in the run function on how to find relevant information for metrics

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -5,15 +5,152 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"time"
+	"encoding/json"
 	"os"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 	"github.com/spf13/cobra"
 	"github.com/joho/godotenv"
 )
 
+type GitHubResponse struct {
+	ID       int    `json:"id"`
+	NodeID   string `json:"node_id"`
+	Name     string `json:"name"`
+	FullName string `json:"full_name"`
+	Private  bool   `json:"private"`
+	Owner    struct {
+		Login             string `json:"login"`
+		ID                int    `json:"id"`
+		NodeID            string `json:"node_id"`
+		AvatarURL         string `json:"avatar_url"`
+		GravatarID        string `json:"gravatar_id"`
+		URL               string `json:"url"`
+		HTMLURL           string `json:"html_url"`
+		FollowersURL      string `json:"followers_url"`
+		FollowingURL      string `json:"following_url"`
+		GistsURL          string `json:"gists_url"`
+		StarredURL        string `json:"starred_url"`
+		SubscriptionsURL  string `json:"subscriptions_url"`
+		OrganizationsURL  string `json:"organizations_url"`
+		ReposURL          string `json:"repos_url"`
+		EventsURL         string `json:"events_url"`
+		ReceivedEventsURL string `json:"received_events_url"`
+		Type              string `json:"type"`
+		SiteAdmin         bool   `json:"site_admin"`
+	} `json:"owner"`
+	HTMLURL          string      `json:"html_url"`
+	Description      interface{} `json:"description"`
+	Fork             bool        `json:"fork"`
+	URL              string      `json:"url"`
+	ForksURL         string      `json:"forks_url"`
+	KeysURL          string      `json:"keys_url"`
+	CollaboratorsURL string      `json:"collaborators_url"`
+	TeamsURL         string      `json:"teams_url"`
+	HooksURL         string      `json:"hooks_url"`
+	IssueEventsURL   string      `json:"issue_events_url"`
+	EventsURL        string      `json:"events_url"`
+	AssigneesURL     string      `json:"assignees_url"`
+	BranchesURL      string      `json:"branches_url"`
+	TagsURL          string      `json:"tags_url"`
+	BlobsURL         string      `json:"blobs_url"`
+	GitTagsURL       string      `json:"git_tags_url"`
+	GitRefsURL       string      `json:"git_refs_url"`
+	TreesURL         string      `json:"trees_url"`
+	StatusesURL      string      `json:"statuses_url"`
+	LanguagesURL     string      `json:"languages_url"`
+	StargazersURL    string      `json:"stargazers_url"`
+	ContributorsURL  string      `json:"contributors_url"`
+	SubscribersURL   string      `json:"subscribers_url"`
+	SubscriptionURL  string      `json:"subscription_url"`
+	CommitsURL       string      `json:"commits_url"`
+	GitCommitsURL    string      `json:"git_commits_url"`
+	CommentsURL      string      `json:"comments_url"`
+	IssueCommentURL  string      `json:"issue_comment_url"`
+	ContentsURL      string      `json:"contents_url"`
+	CompareURL       string      `json:"compare_url"`
+	MergesURL        string      `json:"merges_url"`
+	ArchiveURL       string      `json:"archive_url"`
+	DownloadsURL     string      `json:"downloads_url"`
+	IssuesURL        string      `json:"issues_url"`
+	PullsURL         string      `json:"pulls_url"`
+	MilestonesURL    string      `json:"milestones_url"`
+	NotificationsURL string      `json:"notifications_url"`
+	LabelsURL        string      `json:"labels_url"`
+	ReleasesURL      string      `json:"releases_url"`
+	DeploymentsURL   string      `json:"deployments_url"`
+	CreatedAt        time.Time   `json:"created_at"`
+	UpdatedAt        time.Time   `json:"updated_at"`
+	PushedAt         time.Time   `json:"pushed_at"`
+	GitURL           string      `json:"git_url"`
+	SSHURL           string      `json:"ssh_url"`
+	CloneURL         string      `json:"clone_url"`
+	SvnURL           string      `json:"svn_url"`
+	Homepage         interface{} `json:"homepage"`
+	Size             int         `json:"size"`
+	StargazersCount  int         `json:"stargazers_count"`
+	WatchersCount    int         `json:"watchers_count"`
+	Language         string      `json:"language"`
+	HasIssues        bool        `json:"has_issues"`
+	HasProjects      bool        `json:"has_projects"`
+	HasDownloads     bool        `json:"has_downloads"`
+	HasWiki          bool        `json:"has_wiki"`
+	HasPages         bool        `json:"has_pages"`
+	HasDiscussions   bool        `json:"has_discussions"`
+	ForksCount       int         `json:"forks_count"`
+	MirrorURL        interface{} `json:"mirror_url"`
+	Archived         bool        `json:"archived"`
+	Disabled         bool        `json:"disabled"`
+	OpenIssuesCount  int         `json:"open_issues_count"`
+	License          struct {
+		Key    string `json:"key"`
+		Name   string `json:"name"`
+		SpdxID string `json:"spdx_id"`
+		URL    string `json:"url"`
+		NodeID string `json:"node_id"`
+	} `json:"license"`
+	AllowForking             bool          `json:"allow_forking"`
+	IsTemplate               bool          `json:"is_template"`
+	WebCommitSignoffRequired bool          `json:"web_commit_signoff_required"`
+	Topics                   []interface{} `json:"topics"`
+	Visibility               string        `json:"visibility"`
+	Forks                    int           `json:"forks"`
+	OpenIssues               int           `json:"open_issues"`
+	Watchers                 int           `json:"watchers"`
+	DefaultBranch            string        `json:"default_branch"`
+	Permissions              struct {
+		Admin    bool `json:"admin"`
+		Maintain bool `json:"maintain"`
+		Push     bool `json:"push"`
+		Triage   bool `json:"triage"`
+		Pull     bool `json:"pull"`
+	} `json:"permissions"`
+	TempCloneToken            string `json:"temp_clone_token"`
+	AllowSquashMerge          bool   `json:"allow_squash_merge"`
+	AllowMergeCommit          bool   `json:"allow_merge_commit"`
+	AllowRebaseMerge          bool   `json:"allow_rebase_merge"`
+	AllowAutoMerge            bool   `json:"allow_auto_merge"`
+	DeleteBranchOnMerge       bool   `json:"delete_branch_on_merge"`
+	AllowUpdateBranch         bool   `json:"allow_update_branch"`
+	UseSquashPrTitleAsDefault bool   `json:"use_squash_pr_title_as_default"`
+	SquashMergeCommitMessage  string `json:"squash_merge_commit_message"`
+	SquashMergeCommitTitle    string `json:"squash_merge_commit_title"`
+	MergeCommitMessage        string `json:"merge_commit_message"`
+	MergeCommitTitle          string `json:"merge_commit_title"`
+	SecurityAndAnalysis       struct {
+		SecretScanning struct {
+			Status string `json:"status"`
+		} `json:"secret_scanning"`
+		SecretScanningPushProtection struct {
+			Status string `json:"status"`
+		} `json:"secret_scanning_push_protection"`
+	} `json:"security_and_analysis"`
+	NetworkCount     int `json:"network_count"`
+	SubscribersCount int `json:"subscribers_count"`
+}
 
 func getEnvVar(key string) string {
 	err := godotenv.Load(".env")
@@ -35,6 +172,8 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
+
+	// test with go run main.go -- https://api.github.com/repos/anthony-pei/ECE461
 	Run: func(cmd *cobra.Command, args []string) { 
 		client := &http.Client{}
 		req, err := http.NewRequest("GET", args[0], nil)
@@ -50,12 +189,35 @@ to quickly create a Cobra application.`,
 		}
 		defer resp.Body.Close()
 		
-		bodyText, err := ioutil.ReadAll(resp.Body)
+		bodyText, err := io.ReadAll(resp.Body)
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("%stest\n", bodyText)
 
+		var response GitHubResponse 
+		json.Unmarshal(bodyText, &response)
+
+
+		// check out https://api.github.com/repos/anthony-pei/ECE461 for more info
+		// Ramp up: (# of lines of comments / # of lines of total code) / .2 
+		// check contents_url, comments_url
+
+		// Correctness 1 - (# of issues / # of stars)
+		fmt.Printf("star count: %v\n", response.StargazersCount) // check issues_url
+		fmt.Printf("open issues count: %v\n", response.open_issues_count)
+
+		// Bus Factor: 1 - (1 / (# of contributors)) 
+		// response.contributors_url + count 
+
+		// Responsiveness: 1 - .01 * (open time (days) of last 10 closed issues)
+		// check issues_url
+
+		// License
+		// response.license.name or .key, create a table of valid licenses
+
+		// Call netscore function (weights[], scores[])
+
+		// print url + netscore
 	},
 }
 
