4f1f418446d258ef982e14257854b98c1108d0a5
Mark <mbaumste@purdue.edu>
2023-02-04 13:17:44 -0500

    Refactoring functions in to seperate packages

--- cli/cmd/run.go
+++ cli/cmd/run.go
@@ -4,49 +4,13 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
-	"strings"
 
+	"github.com/anthony-pei/ECE461/cli/file_handler"
 	"github.com/anthony-pei/ECE461/cli/github_util"
 	"github.com/anthony-pei/ECE461/cli/metrics"
 	"github.com/spf13/cobra"
 )
-
-func calcNetScore(modules []metrics.Module) {
-	correctnessMetric := metrics.CorrectnessMetric{}
-	licenseMetric := metrics.LicenseMetric{}
-	for _, module := range modules {
-		fmt.Println(module.GetName())
-		correcntess := correctnessMetric.CalculateScore(module)
-		license := licenseMetric.CalculateScore(module)
-		fmt.Println(correcntess)
-		fmt.Println(license)
-	}
-
-}
-
-func getOwnersNamesFromFile(filename string) []github_util.OwnerName {
-	file, err := os.Open(filename)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	var ownerNames []github_util.OwnerName
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		link := scanner.Text()
-		parts := strings.Split(link, "/")
-		if parts[2] != "www.npmjs.com" {
-			ownerNames = append(ownerNames, github_util.OwnerName{Owner: parts[3], Name: parts[4], Url: link})
-		}
-	}
-	return ownerNames
-}
 
 // runCmd represents the run command
 var runCmd = &cobra.Command{
@@ -54,9 +18,13 @@ var runCmd = &cobra.Command{
 	Short: "A brief description of your command",
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		names := getOwnersNamesFromFile(args[0]) // Not error checking file name
+		names := file_handler.GetOwnersNamesFromFile(args[0]) // Not error checking file name
 		modules := github_util.GetGithubModules(names)
-		calcNetScore(modules)
+		for _, module := range modules {
+			netscoreModule := metrics.NetScoreMetric{}
+			netscore := netscoreModule.CalculateScore(module)
+			fmt.Println(netscore)
+		}
 	},
 }
 
