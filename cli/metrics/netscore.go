736ad20f13960907252d4084b71ecac0e6c16794
Mark <mbaumste@purdue.edu>
2023-02-04 13:40:27 -0500

    Refactoring netscore structure to be compatible with ndjson output

--- cli/metrics/netscore.go
+++ cli/metrics/netscore.go
@@ -1,15 +1,20 @@
 package metrics
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
 
 // If more metric scores are needed, add score into this struct, and call appropriate methods in CalculateScore()
 type NetScoreMetric struct {
-	correctness    float64
-	license        float64
-	busfactor      float64
-	rampup         float64
-	responsiveness float64
-	netscore       float64
+	URL            string  `json:"url"`
+	Correctness    float64 `json:"correctness"`
+	License        float64 `json:"license"`
+	Busfactor      float64 `json:"busfactor"`
+	Rampup         float64 `json:"ramp_up"`
+	Responsiveness float64 `json:"responsive_maintainer"`
+	Netscore       float64 `json:"net_score"`
 }
 
 var correctnessMetric CorrectnessMetric
@@ -22,13 +27,21 @@ func (l NetScoreMetric) CalculateScore(m Module) float64 {
 	// Object l of type license matrix and m of type module with function get_url()
 
 	fmt.Println(m.GetName())
-	l.correctness = correctnessMetric.CalculateScore(m)
-	l.license = licenseMetric.CalculateScore(m)
-	l.busfactor = busfactorMetric.CalculateScore(m)
-	l.rampup = rampUpMetric.CalculateScore(m)
-	l.responsiveness = responsivnessMetric.CalculateScore(m)
+	l.Correctness = correctnessMetric.CalculateScore(m)
+	l.License = licenseMetric.CalculateScore(m)
+	l.Busfactor = busfactorMetric.CalculateScore(m)
+	l.Rampup = rampUpMetric.CalculateScore(m)
+	l.Responsiveness = responsivnessMetric.CalculateScore(m)
 
-	l.netscore = 0.4*l.responsiveness + 0.1*l.rampup + 0.2*l.busfactor + 0.2*l.license + 0.1*l.correctness
+	l.Netscore = 0.4*l.Responsiveness + 0.1*l.Rampup + 0.2*l.Busfactor + 0.2*l.License + 0.1*l.Correctness
 	fmt.Println("Calculating Netscore metric for module:", m.GetGitHubUrl())
-	return l.netscore
+	return l.Netscore
+}
+
+func (l NetScoreMetric) ToNDJson() string {
+	b, err := json.Marshal(l)
+	if err != nil {
+		log.Fatal("Error with NDJson conversion")
+	}
+	return string(b)
 }
