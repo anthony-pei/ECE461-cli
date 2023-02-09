9db11e34055bbb251adad3f24c6eb6bf7c2994fd
Anthony Pei <peiantho@gmail.com>
2023-02-09 17:54:38 -0500

    updated install subcommand

--- run.sh
+++ run.sh
@@ -5,9 +5,10 @@ subdir="cli"
 cd "$subdir"
 if [[ "$1" == "build" ]]; then
   go build
-  echo "built cli program"
+  echo "Built cli program. Type /run <replace with inputFile.txt> to run"
 elif [[ "$1" == "install" ]]; then
-  echo "install dependencies"
+  go build
+  echo "Installed all dependencies"
 elif [[ "$1" == "test" ]]; then
   echo "running tests"
 elif [[ -n "$1" ]]; then
