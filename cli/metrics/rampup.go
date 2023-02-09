eadf8d4be93b7f4536d82861e875fb2d654a292b
Mark <mbaumste@purdue.edu>
2023-02-08 20:49:54 -0500

    Cleaning up file after download

--- cli/metrics/rampup.go
+++ cli/metrics/rampup.go
@@ -2,6 +2,8 @@ package metrics
 
 import (
 	"fmt"
+	"log"
+	"os"
 )
 
 type RampUpMetric struct {
@@ -11,9 +13,14 @@ func (l RampUpMetric) CalculateScore(m Module) float64 {
 	// Object l of type license matrix and m of type module with function get_url()
 
 	dir := "temp"
+	os.RemoveAll(dir)
+
 	m.Clone(dir)
 	// Analyze code
-	// Clean dir
+	err := os.RemoveAll(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println("Calculating ramp up metric for module:", m.GetGitHubUrl())
 	return 0.0
 }
