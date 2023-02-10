be48ffcc617f3c2cca4aeb4667718833b166229e
Anthony Pei <peiantho@gmail.com>
2023-02-09 20:14:44 -0500

    ./run.sh tests

--- run.sh
+++ run.sh
@@ -5,10 +5,12 @@ subdir="cli"
 cd "$subdir"
 if [[ "$1" == "build" ]]; then
   go build
-  echo "built cli program"
+  echo "Built cli program. Type /run <replace with inputFile.txt> to run"
 elif [[ "$1" == "install" ]]; then
-  echo "install dependencies"
+  go build
+  echo "Installed all dependencies"
 elif [[ "$1" == "test" ]]; then
+  go test ./...
   echo "running tests"
 elif [[ -n "$1" ]]; then
   echo "checking URL"
