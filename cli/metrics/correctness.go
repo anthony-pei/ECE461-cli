4d9f4ed8852f3679006bff50a34cb09b0355f776
Anthony Pei <peiantho@gmail.com>
2023-02-11 17:34:23 -0500

    resolve division by zero

--- cli/metrics/correctness.go
+++ cli/metrics/correctness.go
@@ -11,7 +11,7 @@ type CorrectnessMetric struct {
 func (l CorrectnessMetric) CalculateScore(m Module) float64 {
 	// Object l of type license matrix and m of type module with function get_url()
 	log.Println("Calculating correctness metric for module:", m.GetGitHubUrl())
-	correctness := 1.0 - (0.00001 + float64(m.GetOpenIssuesCount())/float64(m.GetStargazerCount()))
+	correctness := 1.0 - (0.00001 + float64(m.GetOpenIssuesCount())) / (0.00001 + float64(m.GetStargazerCount()))
 	correctness = math.Min(correctness, 1.0)
 	correctness = math.Max(correctness, 0.0)
 	return correctness
